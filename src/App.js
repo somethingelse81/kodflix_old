861a8c2c23871c462d650504d3b95e8d0240b0a2
Sanj <sb@Sanjs-MBP.localdomain>
2019-08-07 11:31:40 +0100

    Replaced TV show titles with covers(images)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import avengersendgame from './avengersendgame.jpeg';
-import './App.css';
+import React from "react";
+import blackmirror from "./blackmirror.jpg";
+import avengersendgame from "./avengersendgame.jpeg";
+import breakingbad from "./breakingbad.jpg";
+import deathnote from "./deathnote.jpg";
+import thewalkingdead from "./thewalkingdead.jpg";
+import got from "./got.jpg";
+import thewire from "./thewire.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <img src={avengersendgame} className="avengersendgamecover" alt="avengers end game movie cover" />
+      <img
+        src={avengersendgame}
+        className="avengersendgamecover"
+        alt="avengers end game movie cover"
+      />
       <div className="container">
         <ul className="row">
-          <li className="card"><h1>Black Mirror</h1></li>
-          <li className="card"><h1>Breaking Bad</h1></li>
-          <li className="card"><h1>Death Note</h1></li>
+          <li className="card">
+            <img src={blackmirror} alt="Black Mirror TV show cover" />
+          </li>
+          <li className="card">
+            <img src={breakingbad} alt="Breaking Bad show cover" />
+          </li>
+          <li className="card">
+            <img src={deathnote} alt="Death Note show cover" />
+          </li>
         </ul>
         <ul className="row">
-          <li className="card"><h1>Game of Thrones</h1></li>
-          <li className="card"><h1>The Walking Dead</h1></li>
-          <li className="card"><h1>The Wire</h1></li>
-        </ul></div>
-
+          <li className="card">
+            <img src={got} alt="Game Of Thrones show cover" />
+          </li>
+          <li className="card">
+            <img src={thewalkingdead} alt="The Walking Dead show cover" />
+          </li>
+          <li className="card">
+            <img src={thewire} alt="The Wire show cover" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
